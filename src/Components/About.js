d9af72d763b59fdea4ffbd65a24b34c12b4a05ba
Clouloayza <clou7loymed@gmail.com>
2017-12-21 14:21:59 -0500

    modificaciones

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,14 +14,16 @@ export const About = () => {
             </Col>
         <Col md={5} sm={12} xs={12} lg={5}>
                 <div>
-                    <img src={perfil2} alt="" className="perfil2" responsive/>
+                    <img src={perfil2} alt="" className="perfil2 text-center" responsive/>
                 </div>
             </Col>
             <Col md={5} sm={12} xs={12} lg={5} className="pull-left">
-                <div className="description">
+                <div className="description container">
                     <h1 className="title text-center">Sobre mi</h1>
-                    <p>Soy Front-end Developer gracias a la oportunidad que obtuve cursando estudios de programación en el bootcamp que ofrece <a target="_blank" href="http://www.laboratoria.la/"> Laboratoria</a>. En el transcurso del programa he logrado aprender HTML, CSS, JS y REACT.
-                    Hace un tiempo culmine mi carrera universitaria de Marketing, obteniendo una formación integral, logrando canalizar en el presente mis capacidades y habilidades, innatas y adquiridas, para lograr alcanzar las metas propuestas en el sector tecnológico. Con Laboratoria he logrado descubrir mi verdadera pasión hacia la programación, diseño y organización de proyectos en equipo. Soy consciente del mundo de oportunidades a la que actualmente las mujeres podemos acceder, equilibrando nuestras oportunidades con nuestras capacidades, y quiero ser parte del gran cambio que este mundo ofrece a nuestra sociedad
+                    <p className="parrafo text-center">Soy Front-end Developer gracias a la oportunidad que obtuve cursando estudios de programación en el bootcamp que ofrece <a target="_blank" href="http://www.laboratoria.la/"> Laboratoria</a>. En el transcurso del programa he logrado aprender HTML, CSS, JS y REACT.
+                    Culmine mi carrera universitaria de Marketing. 
+                    Obtengo una formación integral, logré canalizar mis capacidades y habilidades, innatas y adquiridas, para lograr alcanzar las metas propuestas en el sector tecnológico. 
+                    Con Laboratoria he logrado descubrir mi verdadera pasión hacia la programación, diseño y organización de proyectos en equipo. Soy consciente del mundo de oportunidades a la que actualmente las mujeres podemos acceder, equilibrando nuestras oportunidades con nuestras capacidades, y quiero ser parte del gran cambio que este mundo ofrece a nuestra sociedad
                     </p>
                     <div className="icons text-center">
                         
--- src/Components/Icono.js
+++ src/Components/Icono.js
@@ -2,15 +2,13 @@ import React, {Component} from 'react';
 import '../css/main.css';
 import {Col, Grid, Row, Image} from 'react-bootstrap';
 import icono from '../Img/icono.png';
-export const Footer = () => {
+export const Icono = () => {
     return (
-        <section className="footer text-center" id="icono">
+        <section className="icono text-center" id="icono">
                 <Col md={12} sm={12} xs={12} lg={12}>
+                
                     <img src={icono} alt="" className="icono text-center" responsive/>
-                    <p>GMAIL: clou7loymed@gmail.com <br/>
-                    <span>CELL: 989411469</span>
-                    </p>
-                    
+                
                 </Col>
         </section>
     );
